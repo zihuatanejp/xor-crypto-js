c61f1da9b3b0cf57361df4450f3fc27c37f01e68
zihuatanejp <aimilixi@gmail.com>
2018-12-06 13:21:36 +0800

    update pack

--- index.js
+++ index.js
@@ -1,117 +1,120 @@
 'use strict';
 
+(function(f){
+    var def = f();
+    var gbenv = {};
 
-var log = console.log;
-var def = {
-    enc_xor: enc_xor,   //加密方法 传入两个必填参数 1:原文字符串  2.密钥字符串  返回：密文
-    dec_xor: dec_xor  // 解密方法 传入两个必填参数 1.密文 2.密钥字符串   返回：原文
-};
-
-//任意utf8字符串  和任意utf8密钥 的异或加解密
-//返回ciphertext(密文)
-function enc_xor(plain_text, cipher_key) {
-    if (!plain_text || !cipher_key) {
-        return 'nil';
-    }
-    var c = str2hex(cipher_key);
-    var p = str2hex(plain_text);
-    var clen = c.length;
-    var miss = p.length % clen;
-    if (miss != 0) {
-        miss = clen - miss;
-        for (var missi = 0; missi < miss; missi++) {
-            p = p + 'k';
+    try {
+        if (global) {
+            gbenv = global;
+            module.exports = def;
         }
-    }
-    var parr = split_pretty(p, clen);
-    var resarr = parr.map(function(item) {
-        return xor_by_bit(item, c)
-    });
-    return resarr.join('');
-}
-
-// 如果密码错误 返回 'nil'
-function dec_xor(cipher_text, cipher_key) {
-    if (!cipher_text || !cipher_key) {
-        return 'nil';
-    }
-    var ck = str2hex(cipher_key);
-    var ct = cipher_text;
-    var clen = ck.length;
-    var miss = ct.length % clen;
-    if (miss != 0) {
-        return 'nil'
-    }
-    var ct_arr = split_pretty(ct, clen);
-    var resarr = ct_arr.map(function(item) {
-        return xor_by_bit(item, ck)
-    });
-    var res = resarr.join('');
-    res = hex2str(res);
-    return res;
-}
-
-function str2hex(str) {
-    var res = '';
-    for (var i = 0; i < str.length; i++) {
-        res = res + ('k' + str.charCodeAt(i).toString(16));
-    }
-    return res;
-}
-
-function hex2str(str) {
-    var strarr = str.split("k")
-    var res = '';
-    for (var i = 0; i < strarr.length; i++) {
-        if (strarr[i] && parseInt(strarr[i], 16)) {
-            res = res + (String.fromCharCode(parseInt(strarr[i], 16)))
+    } catch (e) {log('global err',e);}
+    try {
+        if (typeof window !== 'undefined' ) {
+            gbenv = window;
         }
-    }
-    return res;
-}
+    } catch (e) { log('window err',e);}
+    gbenv.xor = def;
+})(function(){
+    var log = console.log;
+    var def = {
+        enc_xor: enc_xor,   //加密方法 传入两个必填参数 1:原文字符串  2.密钥字符串  返回：密文
+        dec_xor: dec_xor  // 解密方法 传入两个必填参数 1.密文 2.密钥字符串   返回：原文
+    };
 
-function split_pretty(s, d) {
-    var slen = s.length;
-    if (slen < d) {
-        return 'nil';
-    }
-    var res_arr = [];
-    var arr_item = '';
-    for (var i = 0; i < slen; i++) {
-        arr_item = arr_item + s.charAt(i);
-        if (arr_item.length == d) {
-            res_arr.push(arr_item);
-            arr_item = '';
+    //任意utf8字符串  和任意utf8密钥 的异或加解密
+    //返回ciphertext(密文)
+    function enc_xor(plain_text, cipher_key) {
+        if (!plain_text || !cipher_key) {
+            return 'nil';
         }
+        var c = str2hex(cipher_key);
+        var p = str2hex(plain_text);
+        var clen = c.length;
+        var miss = p.length % clen;
+        if (miss != 0) {
+            miss = clen - miss;
+            for (var missi = 0; missi < miss; missi++) {
+                p = p + 'k';
+            }
+        }
+        var parr = split_pretty(p, clen);
+        var resarr = parr.map(function(item) {
+            return xor_by_bit(item, c)
+        });
+        return resarr.join('');
     }
-    if (arr_item) {
-        res_arr.push(arr_item)
-    }
-    return res_arr;
-}
 
-function xor_by_bit(a, b) {
-    var bits = b.length;
-    var res = '';
-    for (var i = 0; i < bits; i++) {
-        var m = parseInt(a.charAt(i), 36);
-        var k = parseInt(b.charAt(i), 36);
-        res = res + (m ^ k).toString(36);
+    // 如果密码错误 返回 'nil'
+    function dec_xor(cipher_text, cipher_key) {
+        if (!cipher_text || !cipher_key) {
+            return 'nil';
+        }
+        var ck = str2hex(cipher_key);
+        var ct = cipher_text;
+        var clen = ck.length;
+        var miss = ct.length % clen;
+        if (miss != 0) {
+            return 'nil'
+        }
+        var ct_arr = split_pretty(ct, clen);
+        var resarr = ct_arr.map(function(item) {
+            return xor_by_bit(item, ck)
+        });
+        var res = resarr.join('');
+        res = hex2str(res);
+        return res;
     }
-    return res;
-}
 
-var gbenv = {};
+    function str2hex(str) {
+        var res = '';
+        for (var i = 0; i < str.length; i++) {
+            res = res + ('k' + str.charCodeAt(i).toString(16));
+        }
+        return res;
+    }
 
-try {
-    if (global) {
-        gbenv = global;
-        module.exports =def;
+    function hex2str(str) {
+        var strarr = str.split("k")
+        var res = '';
+        for (var i = 0; i < strarr.length; i++) {
+            if (strarr[i] && parseInt(strarr[i], 16)) {
+                res = res + (String.fromCharCode(parseInt(strarr[i], 16)))
+            }
+        }
+        return res;
     }
-} catch (e) {log('global err',e);}
-try {
-    if (typeof window !== 'undefined' ) {
-        gbenv = window;
+
+    function split_pretty(s, d) {
+        var slen = s.length;
+        if (slen < d) {
+            return 'nil';
+        }
+        var res_arr = [];
+        var arr_item = '';
+        for (var i = 0; i < slen; i++) {
+            arr_item = arr_item + s.charAt(i);
+            if (arr_item.length == d) {
+                res_arr.push(arr_item);
+                arr_item = '';
+            }
+        }
+        if (arr_item) {
+            res_arr.push(arr_item)
+        }
+        return res_arr;
     }
-} catch (e) { log('window err',e);}
-gbenv.xor = def;
+
+    function xor_by_bit(a, b) {
+        var bits = b.length;
+        var res = '';
+        for (var i = 0; i < bits; i++) {
+            var m = parseInt(a.charAt(i), 36);
+            var k = parseInt(b.charAt(i), 36);
+            res = res + (m ^ k).toString(36);
+        }
+        return res;
+    }
+    return def;
+});
